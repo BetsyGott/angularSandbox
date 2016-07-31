d9d15451122048532be5fdfb12045858726d62e4
Betsy Gottesman <eightamps@gmail.com>
2016-07-30 23:06:09 -0700

    adding conditional formatting to button component

--- lf_dashboard/angular/components/form_components/buttonComponent.js
+++ lf_dashboard/angular/components/form_components/buttonComponent.js
@@ -2,13 +2,16 @@
     'use strict';
     function buttonComponentController($scope, $element, $attrs) {
         var bcCtrl = this;
-        console.log("button Component Controller");
-
+        bcCtrl.isFirstPage = true;
+        bcCtrl.canCancel = false;
 
     }
 
     angular.module('myApp').component('buttonComponent', {
         templateUrl: 'angular/components/form_components/button_component.html',
-        controller: buttonComponentController
+        controller: buttonComponentController,
+        bindings: {
+            label: "@"
+        }
     });
 })(window.angular);
