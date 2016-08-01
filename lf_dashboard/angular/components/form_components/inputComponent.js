3a0649e684dcf8b3ea029c87d25b4e6706886312
Betsy Gottesman <eightamps@gmail.com>
2016-08-01 12:05:41 -0700

    input component

--- lf_dashboard/angular/components/form_components/inputComponent.js
+++ lf_dashboard/angular/components/form_components/inputComponent.js
@@ -2,23 +2,15 @@
     'use strict';
     function inputComponentController($scope, $element, $attrs) {
         var ctrl = this;
-        // ctrl.editMode = false;
-        //
+
         // ctrl.handleModeChange = function() {
-        //     if (ctrl.editMode) {
+        
         //         ctrl.onUpdate({value: ctrl.fieldValue});
         //         ctrl.fieldValueCopy = ctrl.fieldValue;
-        //     }
-        //     ctrl.editMode = !ctrl.editMode;
+
         // };
-        //
-        // ctrl.reset = function() {
-        //     ctrl.fieldValue = ctrl.fieldValueCopy;
-        // };
-        //
+
         ctrl.$onInit = function() {
-        //     Make a copy of the initial value to be able to reset it later
-        //     ctrl.fieldValueCopy = ctrl.fieldValue;
 
             // Set a default fieldType
             if (!ctrl.fieldType) {
